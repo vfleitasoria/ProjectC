f8a040a67087486e1249031042de1a7463f9e069
vflei <vflei@Vladimir.attlocal.net>
2024-01-04 20:07:49 -0500

    Initial Commit

---
+++ src/ProjectC/DemoC.java
@@ -0,0 +1,14 @@
+package ProjectC;
+
+public class DemoC {
+	
+public static void main(String[]args) {
+		
+		System.out.println("Monday");
+		
+		System.out.println("Git project push...");
+		System.out.println("Git project pull...");
+		
+	}
+
+}
